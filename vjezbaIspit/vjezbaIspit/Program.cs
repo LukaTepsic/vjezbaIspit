0a6a1aa7de93345ae4a11d30f0e14e909e7053e0
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-12-09 12:53:16 +0100

    Add files via upload

---
+++ vjezbaIspit/vjezbaIspit/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaIspit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+            int z = 5;
+
+            if (x > 0 && y > 0 && z > 0)
+            {
+                if (x * x + y * y == z * z)
+                {
+                    Console.WriteLine("Vrijednosti predstavljaju stranice pravokutnog trokuta.");
+                }
+                else
+                {
+                    Console.WriteLine("Vrijednosti ne predstavljaju stranice pravokutnog trokuta.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sve stranice moraju biti veće od 0.");
+            }
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    
+        }
+    }
+
